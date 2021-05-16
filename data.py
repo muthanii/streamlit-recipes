c226b92733e2df31dbb341d60fa1c6307af8c5b7
Muthana Alsaadi <muthanalsaadi@gmail.com>
2021-05-16 12:31:34 +0300

    Added docstrings to finished methods in data.py

--- data.py
+++ data.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-"""
-List of websites:
+"""List of websites:
 1. Epicurious
 
 Problems:
@@ -11,22 +10,26 @@ Problems:
 
 # Epicurious Website Class 
 class Epicurious:
+    
     def __init__(self, search):
-        self.search = search
-
+        self.search = search 
+    
     def get_soup(self):
+        """get_soup() = Get soup (HTML contents that have <a> tag and with wanted class)"""
         src = requests.get(f"https://www.epicurious.com/search/{self.search}").content
         soup = BeautifulSoup(src, "lxml")
         recipes = soup.find_all("a", {"class": "view-complete-item"})
         return recipes
-
+    
     def get_titles(self, soup):
+        """get_titles(soup) = Get titles from a specified soup (search result)"""
         titles = []
         for title in soup:
             titles.append(title.text[4:])
         return titles
 
     def get_links(self, soup):
+        """get_links(soup) = Get links from a specified soup (search result)"""
         links = []
         for link in soup:
             links.append("https://epicurious.com" + link.attrs["href"])
