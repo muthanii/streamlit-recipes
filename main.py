2198a698f2765aeb2d03243b368f53e908e56bc7
Muthana Alsaadi <muthanalsaadi@gmail.com>
2021-05-18 22:54:03 +0300

    Added structure into main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
+import streamlit.components.v1 as components
+from data import Epicurious
+import random
 
-st.title("recipeo")
+TITLE = st.title("recipeo")
+
+USER_INPUT = st.text_input("What are you looking for?")
+    
+e = Epicurious(USER_INPUT)
+
+while USER_INPUT:
+    soup = e.get_soup()
+    links = e.get_links(soup)
+    srcs = e.filter_links(links)
+    i = random.randint(0, len(srcs))
+    components.iframe(srcs[i], height=800, scrolling=True)
